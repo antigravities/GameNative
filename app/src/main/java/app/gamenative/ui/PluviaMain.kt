1050751ad8908a493828dbff5879bef2633e7d99
xXJsonDeruloXx <danielhimebauch@gmail.com>
2026-04-07 10:39:42 -0400

    feat: use game hero image as booting splash background

--- app/src/main/java/app/gamenative/ui/PluviaMain.kt
+++ app/src/main/java/app/gamenative/ui/PluviaMain.kt
@@ -1229,6 +1229,7 @@ fun PluviaMain(
                 BootingSplash(
                     visible = state.showBootingSplash,
                     text = state.bootingSplashText,
+                    heroImageUrl = state.bootingSplashHeroImageUrl,
                 )
             }
 
