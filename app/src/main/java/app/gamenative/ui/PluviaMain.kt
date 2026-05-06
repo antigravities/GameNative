02841f817dc009555b3f2139fb98ae82f102cfad
Alexandra Frock <im@alexandra.moe>
2026-05-05 21:55:50 -0400

    Use game logo instead of "GameNative" text on boot splash screen

--- app/src/main/java/app/gamenative/ui/PluviaMain.kt
+++ app/src/main/java/app/gamenative/ui/PluviaMain.kt
@@ -1270,6 +1270,8 @@ fun PluviaMain(
                     visible = state.showBootingSplash,
                     text = state.bootingSplashText,
                     heroImageUrl = state.bootingSplashHeroImageUrl,
+                    logoUrl = state.bootingLogoImageUrl,
+                    gameName = state.bootingGameName
                 )
             }
 
